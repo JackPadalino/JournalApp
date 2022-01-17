cdb45b91b8b266a85b9b00f9b56c2b76b3a6ad02
Jack <97137486+jackpadalino@users.noreply.github.com>
2022-01-16 19:28:35 -0500

    Added entry detail view
    Users can see details for individual journal entries by clicking on the date of the entry

--- journal/views.py
+++ journal/views.py
@@ -1,7 +1,9 @@
+#from typing_extensions import Required
 from django.shortcuts import render
-from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from .models import Entry
+from django.views.generic import ListView,DetailView,CreateView
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def journalhome(request):
@@ -10,13 +12,37 @@ def journalhome(request):
     }
     return render(request,'journal/home.html',context)
 
+'''
 @login_required
 # in this view we are getting the current authenticated user, then filtering for entries made only by that user
 def journal(request):
     user = request.user
+    #entries = Entry.objects.all()
+    entries = Entry.objects.filter(author=user)
+    entries = entries.order_by('-date_posted')
     context = {
         'title':'My Journal',
-        #'entries':Entry.objects.all(),
-        'entries':Entry.objects.filter(author=user)
+        'entries':entries
     }
     return render(request,'journal/journal.html',context)
+'''
+
+@login_required
+def new_entry(request):
+    context = {
+        'title':'New Entry'
+    }
+    return render(request,'journal/newentry.html',context)
+
+# here we are going to make a 'class view' that will render a page that allows user to CRUD posts
+class EntryListView(ListView):
+    def get_queryset(self):
+        entries = Entry.objects.filter(author=self.request.user)
+        entries = entries.order_by('-date_posted')
+        return entries
+    template_name = 'journal/journal.html'
+    context_object_name = 'entries'
+
+class EntryDetailView(DetailView):
+    model = Entry
+    template_name ='journal/entry.html'
--- users/views.py
+++ users/views.py
@@ -7,17 +7,18 @@ from django.contrib.auth.models import User
 from django.views.generic import ListView
 
 # Create your views here.
+
 def register(request):
-    if request.method=='POST':
+    if request.method == 'POST':
         form = UserRegisterForm(request.POST)
         if form.is_valid():
             form.save()
-            username=form.cleaned_data.get('username')
-            messages.success(request,f'Welcome! Your account has been created and you are now able to log in.')
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Your account has been created! You are now able to log in')
             return redirect('login')
     else:
         form = UserRegisterForm()
-    return render(request,'users/register.html',{'form':form})
+    return render(request, 'users/register.html', {'form': form})
 
 @login_required
 def profile(request):
